e0cfe7cad9585aef18e9bf328884d865ecd0bd97
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-07-01 00:14:18 +0900

    一新

---
+++ Magic/Assets/Magic/Fruit/FruitCreater.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FruitCreater : MonoBehaviour {
+
+    [SerializeField,TooltipAttribute("レーモンのprefabを入れてください")]
+    GameObject lemon = null;
+
+    [SerializeField, TooltipAttribute("アプモンのprefabを入れてください")]
+    GameObject apple = null;
+
+    [SerializeField, TooltipAttribute("モモンのprefabを入れてください")]
+    GameObject peach = null;
+
+    [SerializeField,Range(0,200), TooltipAttribute("レーモンの出す数")]
+    int LEMON_NUM = 100;
+
+    [SerializeField,Range(0,200), TooltipAttribute("アプモンの出す数")]
+    int APPLE_NUM = 100;
+
+    [SerializeField, Range(0,200),TooltipAttribute("モモンの出す数")]
+    int PEACH_NUM = 100;
+
+    HandController hand_controller_ = null;
+
+    void Awake()
+    {
+        hand_controller_ = GameObject.Find("LeapHandController").GetComponent<HandController>();
+
+        LemonCreate(LEMON_NUM);
+        AppleCreate(APPLE_NUM);
+        PeachCreate(PEACH_NUM);
+    }
+
+    void Update()
+    {
+        if (hand_controller_.IsAttack)
+        {
+            LemonCreate(5);
+        }
+    }
+
+    void LemonCreate(int lemon_num)
+    {
+        GameObject lemon_manager = GameObject.Find("LemonManager");
+        for (int i = 0; i < lemon_num; ++i)
+        {
+            if (lemon == null) continue;
+            GameObject game_object = Instantiate(lemon);
+            game_object.transform.SetParent(lemon_manager.transform);
+            game_object.name = lemon.name;
+        }
+    }
+
+    void AppleCreate(int apple_num)
+    {
+        GameObject apple_manager = GameObject.Find("AppleManager");
+        for (int i = 0; i < apple_num; ++i)
+        {
+            if (apple == null) continue;
+            GameObject game_object = Instantiate(apple);
+            game_object.transform.SetParent(apple_manager.transform);
+            game_object.name = apple.name;
+        }
+    }
+
+    void PeachCreate(int peach_num)
+    {
+        GameObject peach_manager = GameObject.Find("PeachManager");
+        for (int i = 0; i < peach_num; ++i)
+        {
+            if (peach == null) continue;
+            GameObject game_object = Instantiate(peach);
+            game_object.transform.SetParent(peach_manager.transform);
+            game_object.name = peach.name;
+        }
+    }
+}
---
+++ Magic/Assets/Magic/LeapMotion/LeapGestureConfig.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeapGestureConfig : MonoBehaviour {
+
+    [SerializeField,Range(0.0f,10.0f), TooltipAttribute("回した時間")]
+    float TURN_SECONDS = 1.5f;
+
+    [SerializeField, Range(0.0f, 3.14f * 2), TooltipAttribute("認識できる範囲(円弧)")]
+    float MIN_ARC = 3.14f * 2;
+
+    [SerializeField, TooltipAttribute("認識できる範囲(半径)")]
+    float MIN_RADIUS = 5.0f;
+
+    public float TurnSeconds { get { return TURN_SECONDS; } }
+    public float MinArc { get { return MIN_ARC; } }
+    public float MinRadius { get { return MIN_RADIUS; } }
+}
