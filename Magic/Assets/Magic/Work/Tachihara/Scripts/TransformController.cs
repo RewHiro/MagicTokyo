180f60f3b3d7bf292387abae9d75c485b8c248d7
tom10987 <tom10987@gmail.com>
2015-07-05 00:40:02 +0900

    UI 表示、位置調整

---
+++ Magic/Assets/Magic/Work/Tachihara/Scripts/TransformController.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+public class TransformController : MonoBehaviour {
+
+  [SerializeField]
+  float scale_ = 0.5f;
+
+  Vector3 neutral_ = Vector3.zero;
+
+  void Start() {
+    transform.rotation = Camera.main.transform.rotation;
+    transform.localScale = Vector3.one * scale_;
+
+    neutral_ = transform.position;
+  }
+
+  void Update() {
+    // TODO: 手で動かしたら元に戻す処理
+  }
+}
