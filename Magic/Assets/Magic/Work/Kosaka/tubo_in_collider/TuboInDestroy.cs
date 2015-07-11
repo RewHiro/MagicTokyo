80bccb9462aa1e5b7aa129d4550d3a0a1dd0a1a3
masashi37 <bantan-gp-mk37@ezweb.ne.jp>
2015-07-11 09:35:08 +0900

    kudamon_get

--- Magic/Assets/Magic/Work/Kosaka/tubo_in_collider/TuboInDestroy.cs
+++ Magic/Assets/Magic/Work/Kosaka/tubo_in_collider/TuboInDestroy.cs
@@ -3,26 +3,40 @@ using System.Collections;
 
 public class TuboInDestroy : MonoBehaviour
 {
+    int lemon_count = 0;
+    int apumon_count = 0;
+    int momon_count = 0;
+
+    public int getLemonCount() { return lemon_count; }
+    public int getApumonCount() { return apumon_count; }
+    public int getMomonCount() { return momon_count; }
+    public int getKudamonCount() {
+        var kudamonAdd = lemon_count + apumon_count + momon_count;
+        return kudamonAdd;
+    }
 
     public void OnTriggerEnter(Collider other)
     {
 
         if (other.name == "re-mon")
         {
-            Debug.Log(" Lemon Destroy ");
             Destroy(other.gameObject, 3);
+            lemon_count++;
+            Debug.Log(" Lemon Destroy " + lemon_count);
         }
 
         if (other.name == "apumon")
         {
-            Debug.Log(" Apple Destroy ");
             Destroy(other.gameObject, 3);
+            apumon_count++;
+            Debug.Log(" Apple Destroy " + apumon_count);
         }
 
         if (other.name == "momon")
         {
-            Debug.Log(" Peach Destroy ");
             Destroy(other.gameObject, 3);
+            momon_count++;
+            Debug.Log(" Peach Destroy " + momon_count);
         }
 
     }
