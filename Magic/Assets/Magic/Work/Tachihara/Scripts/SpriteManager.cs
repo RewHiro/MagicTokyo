dbe89abded53592a921da5648dd21d99ab662b63
tom10987 <tom10987@gmail.com>
2015-07-11 13:59:22 +0900

    UI オブジェクトの当たり判定と初期位置に戻る処理実装

---
+++ Magic/Assets/Magic/Work/Tachihara/Scripts/SpriteManager.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+public class SpriteManager : MonoBehaviour {
+
+  [SerializeField]
+  float shuffle_time_ = 1.0f;
+
+  [SerializeField, Range(1, 10)]
+  int shuffle_speed_ = 1;
+
+  [SerializeField]
+  Sprite[] icon_ = null;
+  Sprite current_ = null;
+
+  GameObject player_ = null;
+
+
+  void Start() {
+    //player_ = FindObjectOfType<Player>();
+  }
+
+  void Update() {
+  }
+
+  public void SetMagic(int magic_type) {
+  }
+}
--- Magic/Assets/Magic/Work/Tachihara/Scripts/TransformController.cs
+++ Magic/Assets/Magic/Work/Tachihara/Scripts/TransformController.cs
@@ -8,7 +8,12 @@ public class TransformController : MonoBehaviour {
   [SerializeField]
   float scale_ = 0.5f;
 
+  [SerializeField]
+  float back_speed_ = 1.0f;
+
   Vector3 neutral_ = Vector3.zero;
+  public Vector3 Neutral { get { return neutral_; } }
+
 
   void Start() {
     transform.rotation = Camera.main.transform.rotation;
@@ -18,6 +23,9 @@ public class TransformController : MonoBehaviour {
   }
 
   void Update() {
-    // TODO: 手で動かしたら元に戻す処理
+    if (Vector3.Equals(transform.position, neutral_)) { return; }
+
+    var dif = (transform.position - Neutral) * back_speed_;
+    transform.position = transform.position - dif * Time.deltaTime;
   }
 }
