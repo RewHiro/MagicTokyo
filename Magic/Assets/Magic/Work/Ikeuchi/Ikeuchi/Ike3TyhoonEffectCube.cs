a260241518b1ae1dcfda2252cae0df48c50b846f
shunta13 <ike3shunta@yahoo.co.jp>
2015-07-24 19:39:38 +0900

    no massage

---
+++ Magic/Assets/Magic/Work/Ikeuchi/Ikeuchi/Ike3TyhoonEffectCube.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ike3TyhoonEffectCube : MonoBehaviour {
+
+    private float angle_ = 0.0f;
+    private float move_value_ = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        angle_ = Random.Range(0.0f, Mathf.PI * 2);
+
+        transform.eulerAngles = new Vector3(
+            Random.Range(0.0f, Mathf.PI * 2),
+            Random.Range(0.0f, Mathf.PI * 2),
+            Random.Range(0.0f, Mathf.PI * 2));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        angle_ += 0.5f;
+        move_value_ += 0.05f;
+        float x = Mathf.Cos(angle_) * move_value_;
+        float y = move_value_ / 10;
+        float z = Mathf.Sin(angle_) * move_value_;
+        transform.position += new Vector3(x, y, z);
+
+        if (move_value_ > 5.0f)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
