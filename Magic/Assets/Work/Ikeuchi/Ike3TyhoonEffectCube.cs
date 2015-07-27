12c3c20096cf2c5bf9134bc3095052390c4aadd9
shunta13 <ike3shunta@yahoo.co.jp>
2015-07-27 18:54:51 +0900

    タイフーンのコードをきれいにした

--- Magic/Assets/Work/Ikeuchi/Ike3TyhoonEffectCube.cs
+++ Magic/Assets/Work/Ikeuchi/Ike3TyhoonEffectCube.cs
@@ -8,26 +8,40 @@ public class Ike3TyhoonEffectCube : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        Init();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        MoveCube();
+        Destroy();
+    }
+
+    void Init()
+    {
         angle_ = Random.Range(0.0f, Mathf.PI * 2);
 
         transform.eulerAngles = new Vector3(
             Random.Range(0.0f, Mathf.PI * 2),
             Random.Range(0.0f, Mathf.PI * 2),
             Random.Range(0.0f, Mathf.PI * 2));
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    void MoveCube()
+    {
         angle_ += 0.5f;
         move_value_ += 0.05f;
         float x = Mathf.Cos(angle_) * move_value_;
         float y = move_value_ / 10;
         float z = Mathf.Sin(angle_) * move_value_;
         transform.position += new Vector3(x, y, z);
+    }
 
+    void Destroy()
+    {
         if (move_value_ > 5.0f)
         {
             Destroy(gameObject);
         }
-	}
+    }
 }
