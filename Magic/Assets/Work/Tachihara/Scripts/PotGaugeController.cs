218940e61b13d27c4f26a89d9865c6c2be2635ea
tom10987 <tom10987@gmail.com>
2015-08-08 22:16:52 +0900

    UI表示位置調整、ナベゲージのスクリプト完了

---
+++ Magic/Assets/Work/Tachihara/Scripts/PotGaugeController.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+
+public class PotGaugeController : MonoBehaviour {
+
+  Transform bar_ = null;
+  float SCALE_Y = 0.0f;
+
+  TuboInDestroy tubo_ = null;
+
+
+  void Start() {
+    bar_ = GameObject.Find("Bar").transform;
+    SCALE_Y = bar_.transform.localScale.y;
+
+    tubo_ = FindObjectOfType<TuboInDestroy>();
+  }
+
+  void Update() {
+    var ratio = tubo_.GetKudamonCount() * 0.1f;
+    var scale = new Vector3(1.0f, SCALE_Y * ratio, 1.0f);
+    bar_.localScale = scale;
+  }
+}
