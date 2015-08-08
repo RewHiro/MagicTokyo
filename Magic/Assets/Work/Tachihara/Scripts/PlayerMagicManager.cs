5d70835d758825ec0f34ac4e047acf88fd95e170
tom10987 <tom10987@gmail.com>
2015-08-09 00:08:58 +0900

    魔法発動中、次の魔法取得を禁止

--- Magic/Assets/Work/Tachihara/Scripts/PlayerMagicManager.cs
+++ Magic/Assets/Work/Tachihara/Scripts/PlayerMagicManager.cs
@@ -2,31 +2,38 @@
 using UnityEngine;
 using UnityEngine.Networking;
 
+
 public class PlayerMagicManager : NetworkBehaviour {
 
   // 使用可能な魔法タイプ（-1: 使用不可）
   // FIXME: 列挙型のほうが信頼性高そう
   public int MagicType { get; private set; }
 
-  SpriteManager sprite_ = null;
+  ItemSpriteManager sprite_ = null;
   TuboInDestroy tubo_ = null;
+
+  readonly int FPS = 60;
+  int cool_time_ = 0;
+
+  [SerializeField, Tooltip("魔法発動後、次の魔法が使用可能になるまでの時間（単位：秒）")]
+  int[] MAGIC_COOL_TIME = null;
 
 
   void Start() {
     if (!isLocalPlayer) return;
     MagicType = -1;
-    sprite_ = FindObjectOfType<SpriteManager>();
+    sprite_ = FindObjectOfType<ItemSpriteManager>();
     tubo_ = FindObjectOfType<TuboInDestroy>();
   }
 
   void Update() {
+    if (IsCoolDown()) { --cool_time_; return; }
+
     if (!isLocalPlayer) return;
     if (!OnGetMomon() || EnableMagic()) { return; }
 
     MagicType = Random.Range(0, sprite_.IconSize - 1);
     sprite_.SlotTriggerEnter();
-
-    //TODO: モモン（と他のくだモン）のカウントを消す処理が必要になると思われる
   }
 
   bool OnGetMomon() {
@@ -38,8 +45,16 @@ public class PlayerMagicManager : NetworkBehaviour {
   }
 
   public void MagicExecute() {
+    // クールダウン中、またはスロット点滅中は発動できない
+    if (IsCoolDown() || sprite_.IsSlotBlink()) return;
+
     sprite_.MagicAction();
+    cool_time_ = FPS * MAGIC_COOL_TIME[MagicType];
     MagicType = -1;
     tubo_.ResetMomon();
+  }
+
+  bool IsCoolDown() {
+    return cool_time_ > 0;
   }
 }
