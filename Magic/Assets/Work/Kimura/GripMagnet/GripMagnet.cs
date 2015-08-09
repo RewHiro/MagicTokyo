a5753dad5ac978790bc067f13a96f48deac84024
kazukin134 <kazukin1345@gmail.com>
2015-08-09 14:52:19 +0900

    チイサクダモンの時間のgetter　とバウンドのイベントの揺れる時間の調整可能にした

---
+++ Magic/Assets/Work/Kimura/GripMagnet/GripMagnet.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GripMagnet : MonoBehaviour {
+
+    delegate void MagicAction();
+
+    PlayerMagicManager player_magic_manager_ = null;
+
+    HandController hand_controller_ = null;
+
+    void Start()
+    {
+
+        hand_controller_ = FindObjectOfType<HandController>();
+
+    }
+
+    void Update()
+    {
+        var magic_type = player_magic_manager_.MagicType;
+
+        foreach (var hand in FindObjectsOfType<SkeletalHand>())
+        {
+            var strength = hand.GetLeapHand().GrabStrength;
+            hand.GetComponentInChildren<Light>().intensity = strength * 8.0f;
+
+            if (1.0f <= strength)
+            {
+                break;
+            }
+        }
+    }
+}
