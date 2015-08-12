1e17b0aa9af6aa5199e7e870ef28ff1c31fbd1d6
U-PS14B22\vantan <akira206@gmail.com>
2015-08-12 13:22:01 +0900

    いらないもの消去

---
+++ Magic/Assets/Work/Yanai/Script/LeapHandSortingLayer.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+
+public class LeapHandSortingLayer : MonoBehaviour {
+
+  void Awake() {}
+
+  void Start() {
+    var hand = GetComponent<SkinnedMeshRenderer>();
+    hand.sortingLayerName = "LeapHand";
+    hand.sortingOrder = 1;
+  }
+
+  void Update() {}
+}
