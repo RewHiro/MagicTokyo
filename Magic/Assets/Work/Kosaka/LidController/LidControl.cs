4db30761177f585ffd56484fd48978871d0ac9f0
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-08-19 02:51:01 +0900

    蓋の修正

---
+++ Magic/Assets/Work/Kosaka/LidController/LidControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LidControl : MonoBehaviour
+{
+
+    Collider lid_collider_;
+    Renderer lid_renderer_;
+
+    public bool can_rendering_lid_ { get; set; }
+
+    //------------------------------------------------
+
+    void Awake()
+    {
+        lid_collider_ = GetComponent<Collider>();
+        lid_renderer_ = GetComponent<Renderer>();
+    }
+
+    void Start()
+    {
+        can_rendering_lid_ = true;
+
+        lid_collider_.isTrigger = false;
+        lid_renderer_.enabled = true;
+    }
+
+    void Update()
+    {
+        //蓋の開け閉め
+        lid_collider_.isTrigger = (can_rendering_lid_) ? false : true;
+        lid_renderer_.enabled = (can_rendering_lid_) ? true : false;
+    }
+
+}
