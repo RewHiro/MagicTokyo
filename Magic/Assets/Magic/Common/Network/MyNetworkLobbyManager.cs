9e6dfdafe6e71227fcde718546047c96692ced6e
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-08-20 19:01:20 +0900

    ネットワークのやり方変更、ScoreManagerのバグ修正

--- Magic/Assets/Magic/Common/Network/MyNetworkLobbyManager.cs
+++ Magic/Assets/Magic/Common/Network/MyNetworkLobbyManager.cs
@@ -7,94 +7,27 @@ public class MyNetworkLobbyManager : NetworkLobbyManager
 {
 
     public static MyNetworkLobbyManager s_singleton = null;
-
-    readonly int TITLE_HASH_CODE = "title".GetHashCode();
-    readonly int GAMEMAIN_HASH_CODE = "gamemain".GetHashCode();
-    readonly int YANAI_TITLE_HASH_CODE = "yanai_title".GetHashCode();
-
-    bool is_ready_ = false;
+    string titel = null;
 
     void Start()
     {
+        titel = Application.loadedLevelName;
         s_singleton = this;
         var textAsset = Resources.Load("connect") as TextAsset;
         JsonNode json = JsonNode.Parse(textAsset.text);
         string ip = json["IP"].Get<string>();
         MyNetworkLobbyManager.singleton.networkAddress = ip;
-    }
 
-    public void GameStart()
-    {
-        if (is_ready_) return;
+
         if (null == MyNetworkLobbyManager.s_singleton.StartHost())
         {
             MyNetworkLobbyManager.s_singleton.StartClient();
         }
-        is_ready_ = true;
-        GameObject.Find("WaitText").GetComponent<Text>().enabled = true;
     }
 
-    public void Stop()
+    public void GameStart()
     {
-        Destroy(gameObject);
-        MyNetworkLobbyManager.s_singleton.StopHost();
-    }
 
-    public override void OnClientDisconnect(NetworkConnection conn)
-    {
-        base.OnClientDisconnect(conn);
-
-        string result = "";
-
-        foreach (var player in FindObjectsOfType<FruitCounter>())
-        {
-            if (!player.isLocalPlayer) continue;
-            var local_fruit_num = player.FruitNum;
-            var remote_fruit_num = player.RemoteFruitNum;
-            if (local_fruit_num == remote_fruit_num)
-            {
-                result = "draw";
-            }
-            else if (local_fruit_num < remote_fruit_num)
-            {
-                result = "win";
-            }
-            else
-            {
-                result = "lose";
-            }
-            var scoresaver = FindObjectOfType<ScoreSaver>();
-            scoresaver.FruitNum = local_fruit_num;
-            scoresaver.RemoteFruitNum = remote_fruit_num;
-            scoresaver.Is1P = false;
-            MyNetworkLobbyManager.s_singleton.StopClient();
-            Application.LoadLevel(result);
-        }
-    }
-
-    void Update()
-    {
-        ChangeScene();
-        var loaded_level_name = Application.loadedLevelName.GetHashCode();
-        if (loaded_level_name == TITLE_HASH_CODE ||
-            loaded_level_name == GAMEMAIN_HASH_CODE ||
-            loaded_level_name == YANAI_TITLE_HASH_CODE)
-            return;
-        Destroy(gameObject);
-    }
-
-    void ChangeScene()
-    {
-        var loaded_level_name = Application.loadedLevelName.GetHashCode();
-        if (!(loaded_level_name == TITLE_HASH_CODE ||
-            loaded_level_name == YANAI_TITLE_HASH_CODE)) return;
-        if (lobbySlots.Length == 0) return;
-        if (lobbySlots[1] == null) return;
-        if (lobbySlots[0].readyToBegin) return;
-        if (lobbySlots[1].readyToBegin)
-        {
-            lobbySlots[0].SendReadyToBeginMessage();
-        }
     }
 
 }
--- Magic/Assets/Magic/Common/Score/ScoreSaver.cs
+++ Magic/Assets/Magic/Common/Score/ScoreSaver.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class ScoreSaver : MonoBehaviour
 {
 
+    readonly int TITLE_HASH_CODE = "title".GetHashCode();
+    readonly int YANAI_TITLE_HASH_CODE = "yanai_title".GetHashCode();
+
     int fruit_num_ = 0;
     public int FruitNum { get { return fruit_num_; } set { fruit_num_ = value; } }
 
     int remote_fruit_num_ = 0;
-    public int RemoteFruitNum { get { return fruit_num_; } set { fruit_num_ = value; } }
+    public int RemoteFruitNum { get { return remote_fruit_num_; } set { remote_fruit_num_ = value; } }
 
     bool is_1p_ = true;
     public bool Is1P { get { return is_1p_; } set { is_1p_ = value; } }
@@ -20,7 +23,10 @@ public class ScoreSaver : MonoBehaviour
 
     void Update()
     {
-        if (Application.loadedLevelName != "title") return;
+        var scene_hash_code = Application.loadedLevelName.GetHashCode();
+        if (!(TITLE_HASH_CODE == scene_hash_code ||
+            YANAI_TITLE_HASH_CODE == scene_hash_code))
+            return;
         Destroy(this);
     }
 }
