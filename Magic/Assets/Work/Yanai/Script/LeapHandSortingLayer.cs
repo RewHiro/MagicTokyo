43df80c4da1e721fda325f8b713fa04de318e733
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-08-21 21:43:32 +0900

    手を手前に描画

--- Magic/Assets/Work/Yanai/Script/LeapHandSortingLayer.cs
+++ Magic/Assets/Work/Yanai/Script/LeapHandSortingLayer.cs
@@ -2,15 +2,27 @@
 using UnityEngine;
 
 
-public class LeapHandSortingLayer : MonoBehaviour {
+public class LeapHandSortingLayer : MonoBehaviour
+{
 
-  void Awake() {}
+    void Awake() { }
 
-  void Start() {
-    //var hand = GetComponent<SkinnedMeshRenderer>();
-    //hand.sortingLayerName = "LeapHand";
-    //hand.sortingOrder = 1;
-  }
+    void Start()
+    {
+        //var hand = GetComponent<SkinnedMeshRenderer>();
+        //hand.sortingLayerName = "LeapHand";
+        //hand.sortingOrder = 1;
+    }
 
-  void Update() {}
+    void Update()
+    {
+        foreach (var finger in FindObjectsOfType<SkeletalFinger>())
+        {
+            foreach (var renderer in finger.GetComponentsInChildren<MeshRenderer>())
+            {
+                renderer.sortingLayerName = "Defult";
+                renderer.sortingOrder = 10;
+            }
+        }
+    }
 }
