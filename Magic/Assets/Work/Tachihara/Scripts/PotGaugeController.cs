c55baf7b75140113500e5486aac399bc6c433547
tom10987 <tom10987@gmail.com>
2015-08-29 15:42:22 +0900

    バグ修正

--- Magic/Assets/Work/Tachihara/Scripts/PotGaugeController.cs
+++ Magic/Assets/Work/Tachihara/Scripts/PotGaugeController.cs
@@ -9,8 +9,8 @@ public class PotGaugeController : MonoBehaviour {
   float SCALE_Y = 0.0f;
 
   TuboInDestroy tubo_ = null;
-  int current_count_ = 0;       // 現在のカウント
-  int accumlation_count_ = 0;   // 累積カウント
+  int current_count_ = 0;   // 現在のカウント
+  int last_count_ = 0;      // 累積カウント
 
   SpriteRenderer effect_ = null;
   Color ALPHA = new Color(1, 1, 1, 0);
@@ -27,22 +27,39 @@ public class PotGaugeController : MonoBehaviour {
   }
 
   void Update() {
-    current_count_ = tubo_.GetKudamonCount() - accumlation_count_;
-    
+    if (IsCountMax()) {
+      effect_.transform.Rotate(new Vector3(0, 0, -Time.deltaTime * 60));
+    }
+
+    // 計算前の値を取得、ナベの中身が増えてなければ処理をスキップ
+    {
+      var prev = current_count_;
+      CountUp();
+      if (prev == current_count_) { return; }
+    }
+    if (current_count_ > 10) { current_count_ = 10; }
+
     var ratio = current_count_ * 0.1f;
     var scale = new Vector3(SCALE_X, SCALE_Y * ratio, 1.0f);
     bar_.localScale = scale;
 
-    var maximum = (current_count_ == 10);
-
-    var set_color = (maximum ? Color.white : ALPHA);
-    if (effect_.color != set_color) { effect_.color = set_color; }
-
-    if (!maximum) { return; }
-    effect_.transform.Rotate(new Vector3(0, 0, -Time.deltaTime * 60));
+    if (IsCountMax() && effect_.color != Color.white) {
+      effect_.color = Color.white;
+    }
   }
 
   public void GaugeReset() {
-    accumlation_count_ += current_count_;
+    // 正しく初期化できるようにカウントを再計算する
+    CountUp();
+    last_count_ += current_count_;
+
+    // 色を戻す
+    effect_.color = ALPHA;
   }
+
+  void CountUp() {
+    current_count_ = tubo_.GetKudamonCount() - last_count_;
+  }
+
+  bool IsCountMax() { return current_count_ == 10; }
 }
