f19e370b011c2a8f2359984572ca9891ef124480
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-09-15 13:35:32 +0900

    最初の音けし

--- Magic/Assets/Magic/Common/Fruit/FruitCreater.cs
+++ Magic/Assets/Magic/Common/Fruit/FruitCreater.cs
@@ -99,8 +99,8 @@ public class FruitCreater : MonoBehaviour {
             GameObject game_object = Instantiate(dorian);
             game_object.transform.SetParent(dorian_manager.transform);
             game_object.name = dorian.name;
+            AudioManager.Instance.PlaySe(7);
         }
-        AudioManager.Instance.PlaySe(7);
     }
 
     public GameObject LemonCreate()
