4f274a8e1c62aba08517c7c613fad637df1256b2
kazukin134 <kazukin1345@gmail.com>
2015-09-15 09:48:00 +0900

    タイトルの演出変更と床のマテリアル変更とバウンド注意報のテロップ差し替え

---
+++ Magic/Assets/Work/Kimura/TitleKudamonArch.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+
+public class TitleKudamonArch : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        GetComponent<Rigidbody>().velocity += new Vector3(0, -100, 0);
+        //transform.position += new Vector3(0, -30, 0);
+        if(transform.position.y < -1000.0f || transform.position.x > 900)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
